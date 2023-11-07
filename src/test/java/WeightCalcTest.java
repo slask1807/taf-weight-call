d016511002f0337312faf6a27284d20f62a79b1f
“Dzmitry <slask@tut.by>
2023-11-07 12:52:13 +0300

    Initial commit

---
+++ src/test/java/WeightCalcTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WeightCalcTest {
+    @Test
+    public void testFormWithEmptyValues() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        By inputCalculateBy = By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input");
+        WebElement inputCalculateWebelement = driver.findElement(inputCalculateBy);
+        inputCalculateWebelement.click();
+
+        By bGetTextBy = By.xpath("/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td/b");
+        WebElement bGetText = driver.findElement(bGetTextBy);
+
+        String expectedText = bGetText.getText();
+        String actualText = ("Не указано имя.\nРост должен быть в диапазоне 50-300 см.\nВес должен быть в диапазоне 3-500 кг.\nНе указан пол.");
+        Assertions.assertEquals(expectedText, actualText);
+    }
+}
